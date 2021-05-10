a5c54f1300c73e6b26be2b750164aa3ef1cfd9cb
nikhilyadav20 <63465752+nikhilyadav20@users.noreply.github.com>
2021-05-10 20:28:06 +0530

    Update wtforms_layout.py

--- wtforms_layout.py
+++ wtforms_layout.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField
-from wtforms.validators import InputRequired, Length, EqualTo
+from wtforms.validators import InputRequired, Length, EqualTo, ValidationError
+from models import User
 
 class RegistrationForm(FlaskForm):
     # Registration Form
@@ -10,3 +11,9 @@ class RegistrationForm(FlaskForm):
     confirm_pswd = PasswordField('confirm_pswd', validators=[InputRequired(message="Password required"), EqualTo('password', message="Passwords must match")])
 
     submit_button = SubmitField('Create')
+
+    def validate_username(self,username):
+        user_object = User.query.filter_by(username=username.data).first()
+        if user_object:
+            raise ValidationError("Username already exists")
+    

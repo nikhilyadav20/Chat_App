8ec57a7db444538ef5d51df0394fc498baea8e6e
nikhilyadav20 <63465752+nikhilyadav20@users.noreply.github.com>
2021-05-10 20:27:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from models import *
 app = Flask(__name__)
 app.secret_key = 'replace later'
 
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://pjecfjuvufvcbi:cb13122faf62bb6a3598546606e943b7ce830939944c8dee2fc96f9e6fbdc75d@ec2-52-0-114-209.compute-1.amazonaws.com:5432/db8l39u420h9p6'
 db = SQLAlchemy(app)
 
 @app.route('/',methods = ['Get','POST'])
@@ -15,10 +16,11 @@ def index():
         username = reg_form.username.data
         password = reg_form.password.data
 
+
         user = User(username=username,password=password)
         db.session.add(user)
         db.session.commit()
-        return "Inserted"
+        return "Inserted into DB"
 
     return render_template('index.html',form=reg_form)
 

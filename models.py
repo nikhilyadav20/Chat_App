abd9c57e3b62de9fdfa4d62fef39baca2a6ed884
nikhilyadav20 <63465752+nikhilyadav20@users.noreply.github.com>
2021-05-14 02:26:52 +0530

    Update models.py

--- models.py
+++ models.py
@@ -3,11 +3,10 @@ from flask_login import UserMixin
 
 db = SQLAlchemy()
 
-
-class User(UserMixin, db.Model):
+class User(UserMixin,db.Model):
     """ User model """
 
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(25), unique=True, nullable=False)
-    hashed_pswd = db.Column(db.String(), nullable=False)
+    password = db.Column(db.String(), nullable=False)
